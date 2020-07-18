d28808f0292b79a0fa1adaa3c56d762c896bb082
yuuaHP <yuuahp@icloud.com>
2020-07-18 22:34:23 +0900

    権限関連実装

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -135,6 +135,15 @@ public class Main extends ListenerAdapter {
             event.getJDA().getTextChannelById("686473875143393288").sendMessage("JYID`"+cutmsg+"`が**否認**されました。").queue();
             //質問相談
         }
+        if(event.getMessage().getContentRaw().startsWith(prefix+"oa")){
+            Role role = event.getGuild().getRoleById(686103774691065871L);
+            Role role2 = event.getGuild().getRoleById(720326371217244321L);
+            String Name = event.getMember().getNickname();
+            String member = event.getAuthor().getId();
+            event.getGuild().addRoleToMember(member, role).queue();
+            event.getGuild().addRoleToMember(member,role2).queue();
+            event.getChannel().sendMessage("サーバールールを読んでいただきありがとうございました！\n**"+Name+"さん**、あなたに**閲覧権限**を付与しました！").queue();
+        }
 
 
     }
