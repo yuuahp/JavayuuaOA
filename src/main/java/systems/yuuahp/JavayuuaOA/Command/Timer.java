00b51e079061d5b6d9227de0069f82c3ae6d828e
yuuaHP <yuuahp@icloud.com>
2020-07-26 09:49:54 +0900

    .

--- src/main/java/systems/yuuahp/JavayuuaOA/Command/Timer.java
+++ src/main/java/systems/yuuahp/JavayuuaOA/Command/Timer.java
@@ -2,10 +2,13 @@ package systems.yuuahp.JavayuuaOA.Command;
 
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.SubscribeEvent;
 
 import java.awt.*;
 
+
 public class Timer {
+@SubscribeEvent
     public void onMessageReceived(MessageReceivedEvent event) {
         String shortprefix = "/";
         if (event.getMessage().getContentRaw().startsWith(shortprefix + "timer")) {
