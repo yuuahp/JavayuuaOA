05489e018bf1c0a5fd9b37e320b1aee310fcb7d5
yuuaHP <yuuahp@icloud.com>
2020-07-26 09:45:49 +0900

    TIMER移行

---
+++ src/main/java/systems/yuuahp/JavayuuaOA/Command/Timer.java
@@ -0,0 +1,34 @@
+package systems.yuuahp.JavayuuaOA.Command;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+import java.awt.*;
+
+public class Timer {
+    public void onMessageReceived(MessageReceivedEvent event) {
+        String shortprefix = "/";
+        if (event.getMessage().getContentRaw().startsWith(shortprefix + "timer")) {
+            String msg = event.getMessage().getContentRaw();
+            String cutmsg = msg.substring(7);
+            int i = Integer.parseInt(cutmsg);
+            int i2 = i*1000;
+
+            EmbedBuilder eb = new EmbedBuilder();
+            eb.setTitle(":stopwatch: Timer Started! ("+i+"s)", null);
+            eb.setColor(new Color(246, 162, 255));
+            event.getChannel().sendMessage(eb.build()).queue();
+
+
+            try {
+                Thread.sleep(i2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            EmbedBuilder eb2 = new EmbedBuilder();
+            eb2.setTitle(":stopwatch: TIME OVER! ("+i+"s)", null);
+            eb2.setColor(new Color(246, 162, 255));
+            event.getChannel().sendMessage(eb2.build()).queue();
+        }
+    }
+}
--- src/main/java/systems/yuuahp/JavayuuaOA/Main.java
+++ src/main/java/systems/yuuahp/JavayuuaOA/Main.java
@@ -354,28 +354,7 @@ public class Main extends ListenerAdapter {
 
 
         }
-        if (event.getMessage().getContentRaw().startsWith(shortprefix + "timer")) {
-            String msg = event.getMessage().getContentRaw();
-            String cutmsg = msg.substring(7);
-            int i = Integer.parseInt(cutmsg);
-            int i2 = i*1000;
 
-            EmbedBuilder eb = new EmbedBuilder();
-            eb.setTitle(":stopwatch: Timer Started! ("+i+"s)", null);
-            eb.setColor(new Color(246, 162, 255));
-            event.getChannel().sendMessage(eb.build()).queue();
-
-
-            try {
-                Thread.sleep(i2);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            EmbedBuilder eb2 = new EmbedBuilder();
-            eb2.setTitle(":stopwatch: TIME OVER! ("+i+"s)", null);
-            eb2.setColor(new Color(246, 162, 255));
-            event.getChannel().sendMessage(eb2.build()).queue();
-        }
 
 
 
