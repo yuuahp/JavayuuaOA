564d2f4449ceffa40ca0a2c78b952a70dfca439c
yuuaHP <yuuahp@icloud.com>
2020-07-27 16:41:35 +0900

    SetStatesFix

--- src/main/java/systems/yuuahp/JavayuuaOA/Command/cmd_SetStates_Play.java
+++ src/main/java/systems/yuuahp/JavayuuaOA/Command/cmd_SetStates_Play.java
@@ -5,17 +5,20 @@ import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.SubscribeEvent;
 
+import javax.security.auth.login.LoginException;
+
 public class cmd_SetStates_Play {
     @SubscribeEvent
-    public void onMessageReceived(MessageReceivedEvent event) {
+    public void onMessageReceived(MessageReceivedEvent event)throws LoginException {
         String shortprefix = "/";
         String prefix = "/jy ";
 
         if (event.getMessage().getContentRaw().startsWith(shortprefix + "state")) {
 
-            event.getChannel().sendMessage("states set!");
+            event.getChannel().sendMessage("states set!").queue();
             JDABuilder jdaBuilder = new JDABuilder();
             jdaBuilder.setActivity(Activity.watching("test"));
+            jdaBuilder.build();
         }
     }
 
